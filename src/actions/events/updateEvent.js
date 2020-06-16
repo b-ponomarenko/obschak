1b9c220468c3ae05d8a68ffe14bca3737ce65032
Пономаренко Богдан <b.ponomarenko@tinkoff.ru>
2020-06-16 17:19:50 +0300

    [W-47] Очищаем стор с покупками когда изменяется количество участников

--- src/actions/events/updateEvent.js
+++ src/actions/events/updateEvent.js
@@ -1,10 +1,16 @@
 import { makeEventsRequest } from '../../core/makeRequest';
 import { eventLoaded } from '../../reducers/event';
 import tap from '@tinkoff/utils/function/tap';
+import { resetPurchases } from '../../reducers/purchase';
 
 export default (payload) => (dispatch) =>
     makeEventsRequest({
         method: `events/${payload.id}`,
         httpMethod: 'PUT',
         payload,
-    }).then(tap(({ event }) => dispatch(eventLoaded(event))));
+    }).then(
+        tap(({ event }) => {
+            dispatch(resetPurchases());
+            dispatch(eventLoaded(event));
+        })
+    );
--- src/reducers/purchase.js
+++ src/reducers/purchase.js
@@ -1,8 +1,11 @@
 import { createEvent, createReducer } from '../core/redux';
 
 export const purchaseLoaded = createEvent('PURCHASE_LOADED');
+export const resetPurchases = createEvent('PURCHASE_RESET');
 
-export default createReducer({}).on(purchaseLoaded, (state, purchase) => ({
-    ...state,
-    [purchase.id]: purchase,
-}));
+export default createReducer({})
+    .on(purchaseLoaded, (state, purchase) => ({
+        ...state,
+        [purchase.id]: purchase,
+    }))
+    .on(resetPurchases, () => ({}));
