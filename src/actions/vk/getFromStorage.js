ba2cc1dd5cc31951cde75824c1baa4b092022fbe
Пономаренко Богдан <b.ponomarenko@tinkoff.ru>
2020-07-21 17:15:21 +0300

    [W-152] Не запрашиваем данные, если нет ключей

--- src/actions/vk/getFromStorage.js
+++ src/actions/vk/getFromStorage.js
@@ -1,8 +1,15 @@
 import { bridge } from '../../core/bridge';
 import { loadFromStorage } from '../../reducers/storage';
+import isEmpty from '@tinkoff/utils/is/empty';
 
 export default () => (dispatch) =>
     bridge
         .send('VKWebAppStorageGetKeys', { count: 100, offset: 0 })
-        .then(({ keys }) => bridge.send('VKWebAppStorageGet', { keys }))
+        .then(({ keys }) => {
+            if (isEmpty(keys)) {
+                return { keys: [] };
+            }
+
+            return bridge.send('VKWebAppStorageGet', { keys });
+        })
         .then(({ keys }) => dispatch(loadFromStorage(keys)));
